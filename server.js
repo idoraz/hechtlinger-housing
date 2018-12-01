f5d35788d8b2349f52288543f2e77f62ba27eb61
Ido <idoraz84@gmail.com>
2018-12-01 02:33:18 +0200

    disable cahce for all http request headers

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ var path = require('path');
 var app = express();
 var zillow = new Zillow(ZILLOW_API_TOKEN);
 
-
+app.set('etag', false); // This will disable cache for all http request headers - no 304 code anymore
 app.use(bodyParser.json({
     limit: '50mb'
 }));
